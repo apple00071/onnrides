000b434f1843550d6e85d733a5bb290c078b33c0
apple00071 <pavankumarv1947@gmail.com>
2025-01-08 09:55:53 +0530

    admin

--- app/(main)/profile/components/DocumentVerification.tsx
+++ app/(main)/profile/components/DocumentVerification.tsx
@@ -66,7 +66,7 @@ const DocumentVerification = ({
         </div>
       ) : documentsSubmitted ? (
         <div className="bg-yellow-50 border border-yellow-200 rounded-md p-4 mb-4">
-          <p className="text-yellow-700">Your documents are under review. We'll notify you once they're verified.</p>
+          <p className="text-yellow-700">Your documents are under review. We&apos;ll notify you once they&apos;re verified.</p>
         </div>
       ) : (
         <div className="bg-blue-50 border border-blue-200 rounded-md p-4 mb-4">
--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -464,7 +464,7 @@ export default function ProfilePage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
               <span>
-                We take our user's privacy very seriously. To learn more, check out our{' '}
+                We take our user&apos;s privacy very seriously. To learn more, check out our{' '}
                 <Link href="/privacy-policy" className="text-[#f26e24] hover:underline">
                   Privacy Policy
                 </Link>
