e193ea410e4ca7269fc2ef8a63e806a362f66d2a
apple00071 <pavankumarv1497@gmail.com>
2025-01-11 19:15:15 +0530

    admin

--- app/(main)/vehicles/[id]/page.tsx
+++ app/(main)/vehicles/[id]/page.tsx
@@ -1,9 +1,11 @@
-
 import VehicleDetailsClient from './VehicleDetailsClient';
+import { Metadata } from 'next';
+import prisma from '../../../lib/prisma';
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  
-  
+export async function generateMetadata({ params: _params }: { params: { id: string } }): Promise<Metadata> {
+  const vehicle = await prisma.vehicle.findUnique({
+    where: { id: _params.id }
+  });
 
   return {
     title: vehicle?.name ? `${vehicle.name} - OnnRides` : 'Vehicle Details - OnnRides',
