476ec35be3384b20cb8cd30577d61d68359e5a66
apple00071 <pavankumarv1497@gmail.com>
2025-01-24 16:03:36 +0530

    fix: update drizzle configuration and dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,19 +3,11 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
-
 export default {
   schema: './lib/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  out: './lib/db/migrations',
+  driver: 'pg',
   dbCredentials: {
-    host: 'ep-long-dream-a6avbuml-pooler.us-west-2.aws.neon.tech',
-    database: 'neondb',
-    user: 'neondb_owner',
-    password: 'fpBXEsTct9g1',
-    ssl: true,
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config; 
